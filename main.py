28f6a55cfbf6d33111809e81f21b91b63cee3ae6
maggieezzat <maggieezzat9@gmail.com>
2021-08-11 15:09:42 +0200

    Revert "remove TTS and MT output"
    This reverts commit 7e996035b649f411b3b6417a1311dad5e1a11195.

--- main.py
+++ main.py
@@ -67,7 +67,6 @@ def complete_tashkeel(sentence):
     return final_sentence
 
 
-"""
 @app.route("/")
 def welcome():
 
@@ -102,9 +101,7 @@ def welcome():
     else:
         print("NO INPUT FILE")
         return render_template("record.html")
-"""
 
-"""
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
 
@@ -390,105 +387,7 @@ def upload():
             diac_sent = session['diac_sent'],
             out_female_file = session['out_female_file'],
             out_male_file = session['out_male_file'])
-"""
 
-
-@app.route("/")
-def welcome():
-
-    if 'in_file' in session.keys():
-        return render_template('record.html', 
-                in_file = session['in_file'], 
-                asr_out = session['asr_out'])
-
-    
-    elif 'error_message' in session.keys():
-        print('error message exists')
-        return render_template("record.html", error_message=session['error_message'])
-
-    else:
-        print("NO INPUT FILE")
-        return render_template("record.html")
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-
-    #do_trans = request.args.get('trans')
-    do_trans = 'true'
-    ######################################
-    session['do_trans'] = do_trans
-
-    filename = 'input_file.wav'
-    audio_data = request.data
-
-    output_dir = str(int(time.time()))
-
-    os.makedirs(output_dir)
-    os.makedirs(os.path.join(output_dir, 'ASR'))
-    
-    session['in_file'] = filename
-
-    save_dir = 'static'
-
-    ###################################### 1. SPEECH RECOGNITION ######################################
-    try:
-        start = time.time()
-        
-        with open(os.path.join(save_dir, filename), 'wb') as f:
-            f.write(audio_data)
-
-        with open(os.path.join(output_dir, 'ASR', filename), 'wb') as f:
-            f.write(audio_data)
-        
-        end = time.time()
-        prep_time = end - start
-        print("PREP Time: " + str(prep_time))
-        
-        start = time.time()
-        url = 'http://espnetdecoding.eastus.cloudapp.azure.com:6000'
-        #url ='bla bla blo'
-        files = {'file': open(os.path.join(save_dir, filename), 'rb')}
-
-        ##########r = rq.post(url, files=files)
-        ##########asr_out = json.loads(r.text)['transcript']
-        asr_out='مرحبا فى مركز الابتكار'
-        
-        
-        with open(os.path.join(output_dir, 'ASR', 'asr_output.txt'), 'w') as f:
-            f.write(asr_out + '\n')
-        
-        
-        session['asr_out'] = asr_out.replace('<صخث>', '***')
-
-        end = time.time()
-        asr_time = end - start
-        
-        print("ASR Time: " + str(asr_time))
-    except:
-        print("ERROR FETCHING ASR OUTPUT")
-        
-        session.pop('in_file', None)
-        session.pop('asr_out', None)
-        session.pop('trans_ch', None)
-        session.pop('trans_en', None)
-        session.pop('trans_fr', None)
-        session.pop('trans_ch_ar', None)
-        session.pop('trans_en_ar', None)
-        session.pop('trans_fr_ar', None)
-        session.pop('diac_sent', None)
-        session.pop('out_female_file', None)
-        session.pop('out_male_file', None)
-        
-
-        session['error_message'] = 'There has been a problem with the ASR output.'
-        #print(session['error_message'])
-        return render_template("record.html", error_message=session['error_message'])
-        #session.pop('error_message', None)
-        #return render_template("record.html")
-    
-    return render_template('record.html', 
-            in_file = session['in_file'], 
-            asr_out = session['asr_out'])
 
 
 
