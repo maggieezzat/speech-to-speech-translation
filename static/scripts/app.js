66434a3821e3bb99830473dfb4ce498090b61615
maggieezzat <maggieezzat9@gmail.com>
2021-08-16 10:19:52 +0200

    removed unecessary lines in app.js

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -17,13 +17,10 @@ var canvas = document.getElementById('visualizer');
 var soundClips = document.getElementById('input-audio');
 var cards = document.querySelectorAll('.card');
 
-// var transOn = document.getElementById('trans-on');
-// var transOff = document.getElementById('trans-off');
 
 var clipContainer;
 var divRow;
 var divCol1;
-//var divCol2;
 
 var mediaRecorder = null;
 var mediaStreamSource = null;
@@ -56,72 +53,6 @@ function fix_dpi() {
 fix_dpi();
 
 
-// function createToggle() {
-
-//   var divContainer = document.createElement('div');
-//   divContainer.setAttribute('class', 'btn-group btn-group-toggle');
-//   divContainer.setAttribute('data-toggle', 'buttons');
-
-//   var onLabel = document.createElement('label');
-//   onLabel.setAttribute('id', 'trans-on-label');
-//   onLabel.setAttribute('class', 'btn btn-secondary active');
-//   onLabel.setAttribute('style', 'width: 7.8rem;height: 3rem; text-align: center; padding-top:0.8rem');
-//   onLabel.innerText = 'Translate On';
-
-//   var offLabel = document.createElement('label');
-//   offLabel.setAttribute('id', 'trans-off-label');
-//   offLabel.setAttribute('class', 'btn btn-secondary active');
-//   offLabel.setAttribute('style', 'width: 7.8rem;height: 3rem; text-align: center; padding-top:0.8rem');
-//   offLabel.innerText = 'Translate Off';
-
-//   var onInput = document.createElement('input');
-//   onInput.setAttribute('type', 'radio');
-//   onInput.setAttribute('id', 'trans-on');
-//   onInput.setAttribute('autocomplete', 'off');
-  
-//   onInput.checked = true;
-//   onLabel.disabled = false;
-//   onLabel.style.background = 'rgb(12, 168, 12)';
-//   offLabel.style.background = '#888888';
-//   offLabel.disabled = true;
-
-//   var offInput = document.createElement('input');
-//   offInput.setAttribute('type', 'radio');
-//   offInput.setAttribute('id', 'trans-off');
-//   offInput.setAttribute('autocomplete', 'off');
-
-//   onInput.change = function() {
-//       console.log("on clicked");
-//       onInput.checked = true;
-//       offInput.checked = false;
-//       console.log(onInput.checked);
-//       console.log(offInput.checked);
-//       onLabel.disabled = false;
-//       onLabel.style.background = 'rgb(12, 168, 12)';
-//       offLabel.style.background = '#888888';
-//       offLabel.disabled = true;
-//   }
-
-//   offInput.change = function() {
-//       console.log("off clicked");
-//       onInput.checked = false;
-//       offInput.checked = true;
-//       console.log(onInput.checked);
-//       console.log(offInput.checked);
-//       onLabel.style.background = '#888888';
-//       onLabel.disabled = true;
-//       offLabel.disabled = false;
-//       offLabel.style.background = 'rgb(168, 48, 12)';
-//   }
-  
-//   onLabel.appendChild(onInput);
-//   offLabel.appendChild(offInput);
-//   divContainer.appendChild(onLabel);
-//   divContainer.appendChild(offLabel);
-
-//   return divContainer;
-// }
-
 var allClips;
 var clipIndex;
 var sentence;
@@ -142,9 +73,6 @@ if (navigator.getUserMedia) {
 
   var onSuccess = function(stream) {
     mediaRecorder = new MediaRecorder(stream);
-    //mediaRecorder.mimeType = 'audio/wav';
-    //mediaRecorder.audioChannels = 1;
-    //mediaRecorder.audioBitsPerSecond = 256000;
     mediaStreamSource = audioCtx.createMediaStreamSource(stream);
     
     
@@ -154,35 +82,6 @@ if (navigator.getUserMedia) {
       {
         cards[i].style.visibility='hidden';
       }
-      ////////////
-      //var clipContainer = document.createElement('article');
-      // divRow = document.createElement('div');
-      // divRow.setAttribute('class', 'row');
-      // divRow.setAttribute('id', 'audio-trans');
-      //divRow.setAttribute('style', 'width: 100%; height:80px');
-      
-      // divCol1 = document.createElement('div');
-      // divCol1.setAttribute('class', 'col-sm-8');
-      // divCol1.setAttribute('id', 'audio-col');
-      // divCol2 = document.createElement('div');
-      // divCol2.setAttribute('class', 'col-sm-3');
-      // divCol2.setAttribute('id', 'toggle-col');
-      // divCol2.setAttribute('style', 'display: inline-block; padding-top: 1rem; ');
-
-      //var toggleDiv  = createToggle();
-      //divCol2.appendChild(toggleDiv);
-
-      // divRow.appendChild(divCol1);
-      //divRow.appendChild(divCol2);
-
-      // clipContainer.appendChild(divRow);
-      //clipContainer.appendChild(audio);
-      
-      //soundClips.innerHTML = '';
-      //soundClips.appendChild(clipContainer);
-
-      //divRow.style.visibility = 'hidden';
-
 
       visualize();
       startRecording();
@@ -192,9 +91,6 @@ if (navigator.getUserMedia) {
     stop.onclick = function() {
       mediaRecorder.stop();
       mediaStreamSource.disconnect();
-      // console.log("Media Recorder State:")
-      // console.log(mediaRecorder.state);
-      // console.log("recorder stopped");
       record.style.background = "";
       record.style.color = ""; 
       record.disabled = false;
@@ -216,38 +112,14 @@ if (navigator.getUserMedia) {
     }
 
     mediaRecorder.onstop = function(e) {
-      // console.log("data available after MediaRecorder.stop() called.");
 
       var clipContainer = document.createElement('article');
-      // var divRow = document.createElement('div');
-      // divRow.setAttribute('class', 'row');
-      // divRow.setAttribute('id', 'audio-trans');
-      // divRow.setAttribute('style', 'width: 100%');
-      
-      // var divCol1 = document.createElement('div');
-      // divCol1.setAttribute('class', 'col-sm-9');
-      // divCol1.setAttribute('id', 'audio-col');
-      // var divCol2 = document.createElement('div');
-      // divCol2.setAttribute('class', 'col-sm-3');
-      // divCol2.setAttribute('id', 'toggle-col');
-      // divCol2.setAttribute('style', 'display: inline-block; padding-top: 1rem; ');
 
       var audio = document.createElement('audio');
       audio.setAttribute('id', 'audio-panel');
      
       clipContainer.classList.add('clip');
       audio.setAttribute('controls', '');
-      //divCol1.appendChild(audio);
-
-      //divRow.style.visibility = 'visible';
-
-      // var toggleDiv  = createToggle();
-      // divCol2.appendChild(toggleDiv);
-
-      // divRow.appendChild(divCol1);
-      // divRow.appendChild(divCol2);
-
-      // clipContainer.appendChild(divRow);
 
       clipContainer.appendChild(audio);
       
@@ -259,45 +131,12 @@ if (navigator.getUserMedia) {
       chunks = [];
       var audioURL = window.URL.createObjectURL(blob);
       audio.src = audioURL;
-      // console.log("recorder stopped");
 
     }
 
     mediaRecorder.ondataavailable = function(e) {
       chunks.push(e.data);
     }
-    // if (transOn != null){
-    //   transOn.change = function() {
-    //     console.log("on clicked");
-    //     transOn.checked = true;
-    //     transOff.checked = false;
-    //     console.log(transOn.checked);
-    //     console.log(transOff.checked);
-    //     var onLabel = document.getElementById("trans-on-label");
-    //     var offLabel = document.getElementById("trans-off-label");
-    //     onLabel.disabled = false;
-    //     onLabel.style.background = 'rgb(12, 168, 12)';
-    //     offLabel.style.background = '#888888';
-    //     offLabel.disabled = true;
-    //   }
-    // }
-    
-    // if (transOff != null){
-    //   transOff.change = function() {
-    //     console.log("off clicked");
-    //     transOn.checked = false;
-    //     transOff.checked = true;
-    //     console.log(transOn.checked);
-    //     console.log(transOff.checked);
-    //     var onLabel = document.getElementById("trans-on-label");
-    //     var offLabel = document.getElementById("trans-off-label");
-    //     onLabel.style.background = '#888888';
-    //     onLabel.disabled = true;
-    //     offLabel.disabled = false;
-    //     offLabel.style.background = 'rgb(168, 48, 12)';
-    //   }
-
-    // }
   
   }
 
@@ -371,10 +210,6 @@ function startRecording() {
   });
   
   mediaRecorder.start();
-  // console.log("Media Recorder State:")
-  // console.log(mediaRecorder.state);
-  // console.log("recorder started");
-  
   upload.disabled = true;
   stop.disabled = false;
   
@@ -410,15 +245,11 @@ function uploadCip() {
   
   var clip = allClips[0];
   // console.log(clip)
-  //var word = clip.querySelector('p').innerText;
 
   clip.style.display = 'True';
 
   var audioBlobUrl = clip.querySelector('audio').src;
   // console.log(audioBlobUrl);
-
-  //var toggle = document.getElementById('trans-on');
-  //var translateOn = toggle.checked;
   
   var xhr = new XMLHttpRequest();
   console.log(audioBlobUrl)
@@ -429,9 +260,7 @@ function uploadCip() {
       var blob = this.response;
       console.log(blob)
       var ajaxRequest = new XMLHttpRequest();
-      //var uploadUrl = '/upload?word=' + word 
       var uploadUrl = '/upload'
-      //var uploadUrl = '/upload?trans=' + translateOn 
       
       ajaxRequest.open('POST', uploadUrl, true);
       ajaxRequest.setRequestHeader('Content-Type', 'application/json');    
